7f0fed8cf2d3a81dde737f903e54c4e6977d8d44
Anakinossas <francescotierno22@gmail.com>
2023-01-17 10:01:22 +0100

    Added some comments.

--- src/main/java/it/zerob/poll/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/it/zerob/poll/config/CustomAuthenticationSuccessHandler.java
@@ -37,17 +37,23 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
+        //Changing redirect page based on role of the user that is logging in
         if(roles.contains("ROLE_ADMIN")) {
-            response.sendRedirect("/home");
+            response.sendRedirect("/home"); //Redirect to page for uploading the Excel file and send emails to users.
         } else {
+            //Get logged user in the session
             User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            Users userLoggedIn = usersRepository.findByUsername(loggedUser.getUsername());
-            Polls idPoll = pollsRepository.findByIdPoll(1L);
+            Users userLoggedIn = usersRepository.findByUsername(loggedUser.getUsername()); //Finds user by giving username
+            Polls idPoll = pollsRepository.findByIdPoll(1L); //Finds the poll by giving its id
+            //Find the number of user that didn't answer the poll
             Requests requests = requestsRepository.requestsDone(idPoll, userLoggedIn);
 
+            //If request is null and the poll is not closed the user will be redirected to the poll page
             if(requests == null && idPoll.getIs_closed().equalsIgnoreCase("0")){
                 response.sendRedirect("/poll");
             } else {
+                //If the user has already completed the poll or if it is closed the user will be redirected
+                //To the login page with an error
                 response.sendRedirect("/login?alreadyDone");
             }
         }
